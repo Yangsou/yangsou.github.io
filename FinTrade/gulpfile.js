3ac00b29c0bdff4e45113fbfb1f90f6157ab61fc
yangsou <namnguyentrung95@gmail.com>
2017-12-03 16:09:50 +0700

    gulp minifity css

--- FinTrade/gulpfile.js
+++ FinTrade/gulpfile.js
@@ -2,7 +2,9 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
 const autoprefixer = require('gulp-autoprefixer');
+
 gulp.task('sass', function () {
   return gulp.src('./assets/sass/main.scss')
     .pipe(sass.sync().on('error', sass.logError))
@@ -10,5 +12,6 @@ gulp.task('sass', function () {
             browsers: ['last 2 versions'],
             cascade: false
         }))
+    .pipe(cssmin())
     .pipe(gulp.dest('./assets/css'));
 });
